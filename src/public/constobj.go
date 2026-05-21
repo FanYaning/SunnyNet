cb4b81eec1c7b0403502c014cf360f1dfaf15187
Qin tian <57537577+qtgolang@users.noreply.github.com>
2026-05-22 00:35:47 +0800

    2026-05-22

--- src/public/constobj.go
+++ src/public/constobj.go
@@ -17,7 +17,7 @@ import (
 	"github.com/qtgolang/SunnyNet/src/websocket"
 )
 
-const SunnyVersion = "2026-05-19"
+const SunnyVersion = "2026-05-22"
 const Information = `
 ------------------------------------------------------
        欢迎使用 SunnyNet 网络中间件 - V` + SunnyVersion + `   
